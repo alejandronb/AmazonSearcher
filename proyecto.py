43de2a0c3983ea3fcee3c89326f68ef1ae850b6f
alejandronb <ale90nb@gmail.com>
2014-04-09 20:41:54 +0200

    comienzo

---
+++ proyecto.py
@@ -0,0 +1,32 @@
+#coding: utf-8
+import requests
+import webbrowser
+from lxml import etree
+import bottle
+
+ID_afiliado = "htttwicomale-21"
+
+clave_acceso = "AKIAIJKV4RAQKPLTDABQ"
+
+clave_secreta = "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"
+
+libro = raw_input("Introduce el libro a buscar: ")
+
+@bottle.route('/')
+def home_page():
+	return "Hola Mundo"
+	
+bottle.debug(True)
+bottle.run(host='localhost')
+
+
+peticion = requests.get("""Service=AWSECommerceService
+Version=2011-08-01
+AssociateTag=htttwicomale-21
+Operation=ItemSearch
+SearchIndex=Books
+Keywords=harry+potter
+Timestamp=2014-04-09T18:20:56.000Z
+AWSAccessKeyId=AKIAIJKV4RAQKPLTDABQ""")
+
+print peticion.text
